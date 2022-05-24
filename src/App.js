24ba447f458ed10ee8bc34fa565afeb17410c363
shahnmah <shahn.mah@gmail.com>
2022-05-24 22:51:01 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Pages/Shared/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
 
 
 function App() {
   return (
     <div>
       <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,35 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/images/logo-blue1.png'
 const Header = () => {
     return (
         <div>
-            <h1>Header</h1>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container">
+                    <a class="navbar-brand" href="#">
+                        <img src={logo} alt="logo" />
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <Link to='/' class="nav-link active fw-bold" aria-current="page">Home</Link>
+                            </li>
+                            <li class="nav-item">
+                            <Link to='/' class="nav-link active fw-bold">About</Link>
+                            </li>
+                            <li class="nav-item">
+                            <Link to='/' class="nav-link active fw-bold">Product</Link>
+                            </li>
+                            <li class="nav-item">
+                            <Link to='/' class="nav-link active fw-bold">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 };
