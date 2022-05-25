415a92b2ab0e0d2a75cf7649ed3de87fd0e3a200
shahnmah <shahn.mah@gmail.com>
2022-05-25 06:05:01 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Pages/Shared/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Footer = () => {
+    return (
+        <div class="mt-5 py-5 bg-light">
+            <footer>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 fw-bold">
+                            <p className='mb-0'>© 2022 Toolsa. All Rights Reserved.</p>
+                        </div>
+                        <div className="col-lg-6 d-flex justify-content-end">
+                            <img className='me-3' height={'32px'} src="https://i.ibb.co/0CP1jpF/facebook-2.png" alt="" />
+                            <img className='me-3' height={'32px'} src="https://i.ibb.co/zXhGyrL/instagram.png" alt="" />
+                            <img className='me-3' height={'32px'} src="https://i.ibb.co/dgpHbRn/twitter.png" alt="" />
+                            <img className='me-3' height={'32px'} src="https://i.ibb.co/0KyWg3w/whatsapp.png" alt="" />
+                            <img className='me-3' height={'32px'} src="https://i.ibb.co/YjPS6wD/linkedin.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
