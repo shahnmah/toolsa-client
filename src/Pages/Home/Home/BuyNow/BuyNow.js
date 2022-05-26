c6370f9a6243b50bdbfbdef419d9bc25af0dc747
shahnmah <shahn.mah@gmail.com>
2022-05-26 15:13:30 +0600

    loaded tool using id

--- src/Pages/Home/Home/BuyNow/BuyNow.js
+++ src/Pages/Home/Home/BuyNow/BuyNow.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const BuyNow = () => {
     const { toolId } = useParams()
+    const [tool, setTool] = useState({});
+    useEffect( ()=>{
+        fetch(`http://localhost:5000/buyNow/${toolId}`)
+        .then(res => res.json())
+        .then(data => setTool(data))
+    },[])
+    
     return (
         <div>
             <h1>Buy now {toolId}</h1>
