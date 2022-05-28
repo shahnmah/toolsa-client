ac8a14544e9369f177fd631a3cbfa1fe32cc0745
shahnmah <shahn.mah@gmail.com>
2022-05-28 19:49:56 +0600

    delete added

--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Table } from 'react-bootstrap';
+import { Button, Modal, Table } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
-
+import { ToastContainer, toast } from 'react-toastify';
 const MyOrders = () => {
     const [user] = useAuthState(auth);
-
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     const [purchaseItems, setPurchaseItems] = useState([]);
+
     useEffect(() => {
         if (user) {
             fetch(`http://localhost:5000/purchase?email=${user.email}`)
@@ -14,6 +17,18 @@ const MyOrders = () => {
                 .then(data => setPurchaseItems(data))
         }
     }, [user])
+    const handleCancel = id => {
+        fetch(`http://localhost:5000/purchase/${id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            window.location.reload();
+        })
+        toast.success("Item Deleted Successfully")
+    }
+
     return (
         <div className='ms-5 w-100'>
             <h2>My Orders: {purchaseItems.length}</h2>
@@ -21,27 +36,40 @@ const MyOrders = () => {
             <Table striped bordered hover>
                 <thead>
                     <tr>
-                        <th>Order #</th>
-                        <th>Product Name</th>
-                        <th>Order AMount</th>
+                        <th>#</th>
+                        <th>Item Name</th>
+                        <th>Amount</th>
                         <th>Status</th>
                         <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        purchaseItems.map((purchaseItem, index) => <tr>
-                            <td>{index+1}</td>
+                        purchaseItems.map((purchaseItem, index) => <tr key={purchaseItem._id}>
+                            <td>{index + 1}</td>
                             <td>{purchaseItem.itemName}</td>
-                            <td>{purchaseItem.amount}</td>
-                            <td>Unpaid</td>
-                            <td>DELETE</td>
+                            <td>{purchaseItem._id}</td>
+                            <td><button className='btn btn-success btn-sm'>Pay Now</button></td>
+                            <td><button onClick={() => handleCancel(purchaseItem._id)} className='btn btn-danger btn-sm'>CANCEL</button></td>
+
                         </tr>)
                     }
-    
+                    {/* <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Are you sure, you want to cancel this order?</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                            <Button variant="danger" onClick={handleClose}>
+                                Cancel
+                            </Button>
+                        </Modal.Footer>
+                    </Modal> */}
                 </tbody>
             </Table>
-
+            <ToastContainer/>
         </div>
     );
 };
